c6700eab858e0ea09caac6d3dfffccb2ab30c280
Martynas Stumbra <m.stumbraa@gmail.com>
2021-04-12 09:11:42 +0300

    Quick fix

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,7 +3,7 @@ export { default as Chart } from './Chart/Chart';
 export { default as Quote } from './Quote/Quote';
 export { default as Header } from './Header/Header';
 export { default as Footer } from './Footer/Footer';
-export { default as NewestEvents } from './Tables/NewestEvents/NewestEvents/NewestEvents';
+export { default as NewestEvents } from './Tables/NewestEvents/NewestEvents';
 export { default as ExpandedText } from './ExpandedText/ExpandedText';
 export { default as AvatarGroup } from './AvatarGroup/AvatarGroup';
 export { default as Empty } from './Empty/Empty';
